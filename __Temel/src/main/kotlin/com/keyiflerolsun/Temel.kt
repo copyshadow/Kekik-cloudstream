947bcc62a54f71212f077f40aa5fef777d6b60c0
copyshadow <copyshadow@gmail.com>
2025-04-09 15:31:25 +0300

    Update Temel.kt

--- __Temel/src/main/kotlin/com/keyiflerolsun/Temel.kt
+++ __Temel/src/main/kotlin/com/keyiflerolsun/Temel.kt
@@ -1,6 +1,6 @@
 // ! Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
 
-package com.keyiflerolsun
+package com.copyshadow
 
 import android.util.Log
 import org.jsoup.nodes.Element
